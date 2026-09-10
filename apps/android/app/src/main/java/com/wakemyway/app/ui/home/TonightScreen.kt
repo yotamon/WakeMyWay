15315a99fcc4ea1f7daca4f144cc88d116b384cc
Yotam Faraggi <yotamon@gmail.com>
2026-09-10 03:08:49 +0200

    fix: import Tonight row weight scope

--- apps/android/app/src/main/java/com/wakemyway/app/ui/home/TonightScreen.kt
+++ apps/android/app/src/main/java/com/wakemyway/app/ui/home/TonightScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
