65ea36f6045fe117dcbbcca9babf51d0f6668995
Yotam Faraggi <yotamon@gmail.com>
2026-09-10 02:43:05 +0200

    Register Roborazzi Gradle plugin

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.roborazzi) apply false
 }
